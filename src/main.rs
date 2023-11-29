6cbf22d323293ae31602a00e86a9d724501ef64b
Matheus Martins <mail@mmartins.dev>
2023-11-29 15:31:29 -0300

    add coverage

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
+
+
 fn main() {
-    let mut v = vec![1,2,3,4,5,6,7,8,9,10];
+    let r = sum(668.5, 668.5);
 
-    v.retain(|&x| x % 2 == 0);
-
-    assert_eq!(v.len(), 5);
-
-    let r = sum(2,2);
-    assert_eq!(r, 4);
+    println!("sum result is: {}", r)
 }
 
-fn sum(a: isize, b: isize) -> isize{
+fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
     a + b
 }
 
@@ -18,8 +15,15 @@ mod tests {
     use crate::sum;
 
     #[test]
-    fn test_sum() {
+    fn test_sum_integer() {
         let result = sum(2,2);
         assert_eq!(result, 4)
+    }
+
+    #[test]
+    fn test_sum_float() {
+        let result = sum(3.5, 5.3);
+
+        assert_eq!(result, 8.8)
     }
 }
